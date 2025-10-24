cc7d20135a1fa70d6c5f80d17f9df13bf736a2b5
Alex <alex.stormwood@bigfootds.com>
2025-10-24 21:50:58 +1100

    feat: fuller skeleton of all routes, not just some routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,30 +5,46 @@ import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    
-    <BrowserRouter>
+	<StrictMode>
 
-      <nav>
+		<BrowserRouter>
 
-      </nav>
-      <Routes>
 
-        <Route path='/' element={<div>TODO!</div>} />
-        <Route path='/about' element={<div>TODO!</div>} />
-        <Route path='/contact' element={<div>TODO!</div>} />
-        <Route path='/articles'>
-          <Route index path='/' element={<div>TODO!</div>} />
-          <Route path='/new' element={<div>TODO!</div>} />
-          <Route path='/:articleId/edit' element={<div>TODO!</div>} />
-          <Route path='/category/:categoryName' element={<div>TODO!</div>} />
-          <Route path='/article/:articleId' element={<div>TODO!</div>} />
+			<Routes>
 
-        </Route>
-        <Route path='/' element={<div>TODO!</div>} />
+				<Route element={<div>TODO! Website common components, e.g. navbar and footer</div>}>
+					<Route index element={<div>TODO! Homepage</div>} />
+					<Route path='about' element={<div>TODO! About page </div>} />
+					<Route path='contact' element={<div>TODO! Contact page</div>} />
 
-      </Routes>
-    </BrowserRouter>
-  
-  </StrictMode>,
+					<Route path='articles'>
+						<Route index element={<div>TODO! Articles index/latest page</div>} />
+						<Route path='new' element={<div>TODO! Articles new page </div>} />
+						<Route path='view/:articleId' element={<div>TODO! Article by ID</div>} />
+						<Route path='edit/:articleId' element={<div>TODO! Article edit page </div>} />
+						<Route path='search/category/:categoryName' element={<div>TODO! Articles list filtered by categoryName</div>} />
+					</Route>
+
+					<Route path='users'>
+						<Route index element={<div>TODO! Redirect to profile page</div>} />
+						<Route path='register' element={<div>TODO! User registration page</div>} />
+						<Route path='login' element={<div>TODO! User login page</div>} />
+						<Route path='profile' element={<div>TODO! Redirect to view/:userId with id from JWT or login page</div>} />
+						<Route path='view/:userId' element={<div>TODO! User profile page</div>} />
+						<Route path='edit/:userId' element={<div>TODO! User profile editing page</div>} />
+					</Route>
+
+					<Route path='admin'>
+						<Route index element={<div>TODO! Admin master page</div>} />
+						<Route path='users' element={<div>TODO! Admin users dashboard</div>} />
+						<Route path='articles' element={<div>TODO! Admin articles dashboard</div>} />
+					</Route>
+
+				</Route>
+
+			</Routes>
+
+		</BrowserRouter>
+
+	</StrictMode>,
 )
